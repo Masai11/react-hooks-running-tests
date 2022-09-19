22d2a570a83e19e27858ac39ee96ba604f5a1a53
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-09-19 14:00:49 +0300

    Completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,8 +3,12 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
+
+    // render(<Article />);
+   
+    //  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
   );
 }
 
